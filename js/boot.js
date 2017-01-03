2ed22e2d208c8eebfdf3b8790aacabf429c6194a
phi16 <phi16.256@gmail.com>
2017-01-03 23:58:03 +0900

    Fix

--- js/boot.js
+++ js/boot.js
@@ -1,13 +1,13 @@
 window.onload = ()=>{
-  Picture.load("left","/res/left.png");
-  Picture.load("right","/res/right.png");
-  Picture.load("standL","/res/standL.png");
-  Picture.load("standR","/res/standR.png");
-  Picture.load("jumpL","/res/jumpL.png");
-  Picture.load("jumpR","/res/jumpR.png");
-  Picture.load("block","/res/block.png");
-  Picture.load("ground","/res/ground.png");
-  Picture.load("flag","/res/flag.png");
+  Picture.load("left","/0103/res/left.png");
+  Picture.load("right","/0103/res/right.png");
+  Picture.load("standL","/0103/res/standL.png");
+  Picture.load("standR","/0103/res/standR.png");
+  Picture.load("jumpL","/0103/res/jumpL.png");
+  Picture.load("jumpR","/0103/res/jumpR.png");
+  Picture.load("block","/0103/res/block.png");
+  Picture.load("ground","/0103/res/ground.png");
+  Picture.load("flag","/0103/res/flag.png");
   Poyo.start("canvas",[Main]);
   var resize = ()=>{
     document.getElementById("canvas").width = document.getElementById("container").clientWidth;
